aed52c89833b4c5899541f6dcad10f396ff9de60
Siiphs <siiphs@outlook.com>
2025-07-07 01:38:36 -0400

    Calcular tiempo en busqueda de suffixarray y espacio

--- src/busqueda.cpp
+++ src/busqueda.cpp
@@ -60,30 +60,35 @@ std::set<int> documentosQueContienen( std::string &P,  std::string &T, std::vect
         int doc = documentoDe(pos, posciones);
         documentos.insert(doc);
     }
-    int veces = r - l;
     return documentos;
 }
 
-int main(){
+int main(int argc, char* argv[]) {
+    // Modo de uso ..data/suffix_arrays/suffixArray_XXX_Y.txt ..data/textos/XXX num_doc patron
     LectorDocumentos lector;
     std::string T;
-    std::vector<int> SA = cargarTxt("../data/suffix_arrays/suffixArray_DNA_5.txt");
+    std::vector<int> SA = cargarTxt(argv[1]);
     std::string rutaT = "../data/textoT.txt";
-    std::string carpetaPath = "../data/textos/DNA";
-    int num_documentos = 5; // Cambia esto por el número de documentos que quieras procesar
+    std::string carpetaPath = argv[2]; // Cambia esto por la ruta de tu carpeta de documentos
+    int num_documentos = std::stoi(argv[3]); // Cambia esto por el número de documentos que quieras procesar
     if (std::filesystem::exists(rutaT)) {
         T = lector.cargarTxt(rutaT);
     } else {
         T = lector.concatenarDocumentosConSeparador(carpetaPath, num_documentos);
         lector.crearTxt(rutaT, T);
     }
-    std::string patron = "AGTATCTA"; // Cambia esto por el patrón que quieras buscar
+    std::string patron = argv[4]; // Cambia esto por el patrón que quieras buscar
     std::vector<int> posiciones = posicionesPeso(T, '$');
 
+    auto start = std::chrono::high_resolution_clock::now();
     std::set<int> documentos = documentosQueContienen(patron, T, SA, posiciones);
- 
-    for(int doc : documentos) {
-        std::cout << "El patrón '" << patron << "' se encuentra en el documento: " << doc << std::endl;
-    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+
+    size_t espacio_bytes = documentos.size() * sizeof(int);
+    double espacio_kb = static_cast<double>(espacio_bytes) / 1024.0;
+    double espacio_mb = espacio_kb / 1024.0;
+
+    std::cout << "SA;" << elapsed.count() << ";" << num_documentos << ";" << espacio_bytes << std::endl;
     return 0;
 }
