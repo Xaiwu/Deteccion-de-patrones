14b4386f01697654aea4e7faea5015f2fc62227b
MVergar4 <167582843+MVergar4@users.noreply.github.com>
2025-07-08 05:31:14 -0400

    Documentado RK.h

--- include/RK.h
+++ include/RK.h
@@ -4,6 +4,13 @@
 #include <vector>
 #include <string>
 
+/**
+ * @brief Realiza la búsqueda del patrón en el texto usando el algoritmo Rabin-Karp
+ * @param patron El patrón a buscar
+ * @param text El texto en el que se busca el patrón
+ * @param coincidencias_doc Vector en el que se almacenan la cantidad de coincidencias que tiene cada documento
+ * @return Cantidad de coincidencias encontradas
+ */
 int rabinKarp(std::string &patron, std::string &text, std::vector<int> &coincidencias_doc);
 
 #endif
